3d137efb015cca288d63c00136a64ce19d01d910
戴兴 <3309201506@qq.com>
2025-04-26 13:23:13 +0800

    先保留一份，怕出事

---
+++ myapi/myapi/settings/constants.py
@@ -0,0 +1,17 @@
+"""这里存放一些默认常量，统一管理"""
+
+# 设置效果常量，增加可读性
+BANNER_LENGTH = 5 # 轮播图数量
+
+HEADER_NAV_LENGTH = 8 # 头部导航数量
+
+FOOTER_NAV_LENGTH = 8 # 底部导航数量
+
+# 短信的有效期[单位：秒]
+SMS_EXPIRE_TIME = 60*4
+
+# 短信的时间间隔[单位：秒]
+SMS_INTERVAL_TIME = 60
+
+# 短信的模版id，测试开发时使用 1
+SMS_TEMPLATE_ID = 1
